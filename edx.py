9ac8692ca672f4766bcac8abcb61aa99d516954b
Jason Zhu <fmy@mail.bistu.edu.cn>
2014-06-19 11:29:08 +0800

    Exception handling changed
    Exception handling changed

--- edx.py
+++ edx.py
@@ -8,13 +8,14 @@ for line in file:
     print info
     log = open('user.log', 'a')
     log.write(info+'\n')
+    log.close()
     my_cmd="sudo -u www-data /edx/bin/python.edxapp /edx/app/edxapp/edx-platform/manage.py lms --settings aws create_user -e %s -p edx" % (line)
     user_cmd_op=commands.getstatusoutput(my_cmd)
     if len(user_cmd_op[1])>515:
+        log = open('user.err', 'a')
         error= "Something wrong when add user:\n"+ line + "\nMessage:\n" +user_cmd_op[1]+ "\n"
         print error
-        log.write(error)
-        break
-    log.close()
+        log.write(info+'\n'+error)
+        log.close()
     time.sleep(2)
 file.close()
