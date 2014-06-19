7040a61553bffb7051e31f061ee2ad6ba48984ef
Jason Zhu <fmy@mail.bistu.edu.cn>
2014-06-19 17:22:15 +0800

    Update edx.py

--- edx.py
+++ edx.py
@@ -9,7 +9,7 @@ for line in file:
     log = open('user.log', 'a')
     log.write(info+'\n')
     log.close()
-    my_cmd="sudo -u www-data /edx/bin/python.edxapp /edx/app/edxapp/edx-platform/manage.py lms --settings aws create_user -e %s -p edx" % (line)
+    my_cmd="sudo -u www-data /edx/bin/python.edxapp /edx/app/edxapp/edx-platform/manage.py lms --settings aws create_user -e %s -p edx " % (line)
     user_cmd_op=commands.getstatusoutput(my_cmd)
     if len(user_cmd_op[1])>515:
         log = open('user.err', 'a')
